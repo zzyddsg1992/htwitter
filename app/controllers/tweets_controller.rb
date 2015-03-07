443961da7288235c9e720403193064096620739d
Zhenyu <zhz142@mail.harvard.edu>
2015-03-07 14:09:22 -0500

    added create action for tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,13 @@ class TweetsController < ApplicationController
 	def new
 		@tweet = Tweet.new
 	end
+	def create
+		@tweet = Tweet.new(tweet_params)
+		@tweet.save
+		render 'new'
+	end
+
+	def tweet_params
+		params. require(:tweet).permit(:content)
+	end
 end
