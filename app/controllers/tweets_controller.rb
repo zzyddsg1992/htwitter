1107ef404b1e4b01c643c2e4e2a254c40c67b222
Zhenyu <zhz142@mail.harvard.edu>
2015-03-07 15:30:55 -0500

    add flash sucess

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,8 +4,11 @@ class TweetsController < ApplicationController
 	end
 	def create
 		@tweet = Tweet.new(tweet_params)
-		@tweet.save
-		render 'new'
+		if @tweet.save
+			flash[:success] = "you have created a tweet"
+			redirect_to new_tweet_path
+		end
+
 	end
 
 	def tweet_params
